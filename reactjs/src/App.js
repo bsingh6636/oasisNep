92f7ff4e791da4c07b07d9fbd86b22d7d63342ed
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2025-08-27 22:27:34 +0530

    loader

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,40 +1,42 @@
-import React, { useState, Suspense } from 'react';
+import React, { useState } from 'react';
+import { Navbar } from './component/Header';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { Footer } from './component/Footer';
 import { Provider } from 'react-redux';
 import reduxstore from './Const/Reduxstore';
-
+import Loader from './component/ui/loader';
 // Lazy imports
-const Navbar = React.lazy(() => import('./component/Header').then(m => ({ default: m.Navbar })));
 const Prices = React.lazy(() => import('./component/Prices'));
 const Error = React.lazy(() => import('./component/Error').then(m => ({ default: m.Error })));
 const Body = React.lazy(() => import('./component/Body').then(m => ({ default: m.Body })));
 const PriceDetails = React.lazy(() => import('./component/PriceDetails').then(m => ({ default: m.PriceDetails })));
 const AboutUs = React.lazy(() => import('./component/AboutUs').then(m => ({ default: m.AboutUs })));
-const Footer = React.lazy(() => import('./component/Footer').then(m => ({ default: m.Footer })));
 const ContactUs = React.lazy(() => import('./component/ContactUs').then(m => ({ default: m.ContactUs })));
 const Update = React.lazy(() => import('./component/Update'));
 const Help = React.lazy(() => import('./component/Help').then(m => ({ default: m.Help })));
 const Cart = React.lazy(() => import('./component/Cart').then(m => ({ default: m.Cart })));
 const AdminRoutes = React.lazy(() => import('./Admin/RoutesAdmin'));
 const HouseHoldCode = React.lazy(() => import('./pages/HouseHoldCode'));
-
 export const MyContext = React.createContext();
 
 export const AppLayout = () => {
-  const [priceListAll, setPriceListAll] = useState([]);
+  
+  const [priceListAll, setPriceListAll] = useState([])
   const [isDarkMode, setIsDarkMode] = useState(false);
-
   return (
-    <div className='bg-gradient-to-br from-gray-200 via-blue-200 to-green-300 min-h-screen'>
+    <div className='bg-gradient-to-br from-gray-200 via-blue-200 to-green-300  min-h-screen'>
+      {/* bg-gradient-to-br from-gray-200 via-blue-200 to-green-300 */}
+
       <Provider store={reduxstore}>
         <MyContext.Provider value={{ priceListAll, setPriceListAll, isDarkMode, setIsDarkMode }} >
-          <Suspense fallback={<div className="text-center p-10">Loading...</div>}>
-            <Navbar />
-            <div>
-              <Outlet />
-            </div>
-            <Footer />
-          </Suspense>
+          <Navbar />
+       
+
+         <React.Suspense fallback={ <Loader /> } >
+           <Outlet />
+         </React.Suspense>
+        
+          <Footer />
         </MyContext.Provider>
       </Provider>
     </div>
@@ -44,11 +46,7 @@ export const AppLayout = () => {
 const Approuter = createBrowserRouter([
   {
     path: '/',
-    element: (
-      <Suspense fallback={<div className="text-center p-10">Loading...</div>}>
-        <AppLayout />
-      </Suspense>
-    ),
+    element: <AppLayout />,
     children: [
       { path: '/', element: <Body /> },
       { path: 'aboutUs', element: <AboutUs /> },
@@ -60,11 +58,12 @@ const Approuter = createBrowserRouter([
       { path: '/prices/:object', element: <PriceDetails /> },
       { path: '/price/:cat', element: <Prices /> },
       { path: 'admin/*', element: <AdminRoutes /> },
-      { path: '/netflixCode', element: <HouseHoldCode /> },
-      { path: '*', element: <Error /> },
+      { path : '/netflixCode' , element : <HouseHoldCode/>},
+      { path: '*', element: <Error /> }
     ],
   },
 ]);
+
 
 function App() {
   return <RouterProvider router={Approuter} />;
