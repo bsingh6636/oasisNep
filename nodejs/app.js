8d21aa4f1a3f43aa48cfa5c787f40f091eab1798
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2025-10-22 00:17:59 +0545

    invalid route fix

--- nodejs/app.js
+++ nodejs/app.js
@@ -40,6 +40,10 @@ app.use((err, req, res, next) => {
    
 });
 
+app.use((req, res, next) => {
+    res.status(404).json({ success : false , message: 'Route not found' });
+})
+
 const server = http.createServer(app);
 initializeSocket(server);
 export default server;
