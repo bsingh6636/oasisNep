39d1a72e4047eaa39bb3eaa210e9b2f51d0a60ed
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2025-10-24 07:38:01 +0545

    cart above

--- reactjs/src/component/FlipkartCartUnified.js
+++ reactjs/src/component/FlipkartCartUnified.js
@@ -224,7 +224,7 @@ const FlipkartCartUnified = () => {
       </div>
 
       {/* Mobile Sticky Bottom Checkout */}
-      <div className="lg:hidden fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 shadow-lg">
+      <div className="lg:hidden fixed sm:bottom-10 bottom-10   left-0 right-0 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 shadow-lg">
         {/* Price Breakdown Toggle */}
         <button
           onClick={() => setShowPriceBreakdown(!showPriceBreakdown)}
