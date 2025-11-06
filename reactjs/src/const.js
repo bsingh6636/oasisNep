a3598737c67a9584be348542a95be701fe70e436
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2025-11-07 02:00:42 +0530

    mock fix

--- reactjs/src/const.js
+++ reactjs/src/const.js
@@ -14,28 +14,28 @@ export const carouselimg = [
 ]
 
 export const Pricelist = [
-    {
-        Id: "1",
-        Name: "Netflix",
-        Info: "Netflix - Stream TV shows and movies anytime, anywhere. Watch on your phone, tablet, laptop, and TV without paying more.",
-        ImageId: "https://cdn.dribbble.com/users/9378043/screenshots/16832559/media/10b207c918d604662e088308d16b366d.png",
-        imagALt : "https://res.cloudinary.com/bsingh6636/image/upload/v1725168600/page/ott%20icons/Netflix_fpns7r.png",
-        category: "ott",
-        status :'shared',
-        plans: {
-            "1": "300",
-            "3": "880",
-            "6": "1700",
-            "12": "3200"
-        },
-    },
+    // {
+    //     Id: "1",
+    //     Name: "Netflix",
+    //     Info: "Netflix - Stream TV shows and movies anytime, anywhere. Watch on your phone, tablet, laptop, and TV without paying more.",
+    //     ImageId: "https://cdn.dribbble.com/users/9378043/screenshots/16832559/media/10b207c918d604662e088308d16b366d.png",
+    //     imagALt : "https://res.cloudinary.com/bsingh6636/image/upload/v1725168600/page/ott%20icons/Netflix_fpns7r.png",
+    //     category: "ott",
+    //     status :'shared',
+    //     plans: {
+    //         "1": "300",
+    //         "3": "880",
+    //         "6": "1700",
+    //         "12": "3200"
+    //     },
+    // },
     {
         Id: "14",
         Name: "Prime Video",
         ImageId: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Amazon_Prime_Video_blue_logo_1.svg/1200px-Amazon_Prime_Video_blue_logo_1.svg.png",
         category: "ott",
         Info: "A subscription video-on-demand service.",
-        status :'shared',
+        status: 'shared',
         plans: {
             "3": "270",
             "6": "450",
@@ -45,7 +45,7 @@ export const Pricelist = [
     {
         Id: "2",
         Name: "Hotstar with Vpn",
-        status :'shared',
+        status: 'shared',
         Info: "Disney+Hotstar with VPN - Enjoy exclusive Disney, Pixar, Marvel, Star Wars, and National Geographic content along with regional Indian movies and TV shows.",
         ImageId: "https://res.cloudinary.com/bsingh6636/image/upload/v1718613898/page/ott%20icons/hotstarIcon.jpg",
         category: "ott",
@@ -54,29 +54,29 @@ export const Pricelist = [
             "12": "2400",
         },
     },
-    {
-        Id: "3",
-        Name: "Spotify Premium",
-        Info: "Spotify Premium - Get unlimited skips, download songs for offline listening, and enjoy high-quality audio without ads.",
-        ImageId: "https://www.bitrefill.com/primg/w350h212/spotify-uk.webp",
-        category: "music",
-        status :'private',
-        plans: {
-            "1": "180",
-            "6": "850",
-            "12": "1400",
-        },
-    },
+    // {
+    //     Id: "3",
+    //     Name: "Spotify Premium",
+    //     Info: "Spotify Premium - Get unlimited skips, download songs for offline listening, and enjoy high-quality audio without ads.",
+    //     ImageId: "https://www.bitrefill.com/primg/w350h212/spotify-uk.webp",
+    //     category: "music",
+    //     status :'private',
+    //     plans: {
+    //         "1": "180",
+    //         "6": "850",
+    //         "12": "1400",
+    //     },
+    // },
     {
         Id: "4",
         Name: "YouTube Premium",
         Info: "YouTube Premium - Watch videos uninterrupted by ads, download content for offline viewing, and access YouTube Music Premium.",
         ImageId: "https://static.digit.in/default/new-youtube-premium-features-1280-6d58ec9cf9.png",
         category: "ott",
-        status :'private',
+        status: 'private',
         plans: {
-           
-           "12": "2000",
+
+            "12": "2000",
         },
     },
     {
@@ -85,7 +85,7 @@ export const Pricelist = [
         Info: "Surfshark VPN - Protect your privacy online with unlimited devices, secure connections, and bypass geo-restrictions.",
         ImageId: "https://www.01net.com/en/app/uploads/2022/08/Surfshark-Review.jpeg",
         category: "vpn",
-        status :'shared',
+        status: 'shared',
         plans: {
             "6": "800",
             "12": "1300",
@@ -111,7 +111,7 @@ export const Pricelist = [
         Info: "Nord VPN - Ensure your online security with military-grade encryption, fast connection speeds, and access to over 5400 servers worldwide.",
         ImageId: "https://img.securityinfowatch.com/files/base/cygnus/siw/image/2023/09/16x9/NordVPN_logo.6500c0f150a53.png?auto=format,compress&fit=crop&q=45&h=667&w=1200",
         category: "vpn",
-        status :'shared',
+        status: 'shared',
         plans: {
             "1": "280",
             "3": "650",
@@ -124,7 +124,7 @@ export const Pricelist = [
         Info: "Windscribe Vpn - Enjoy secure browsing, ad blocking, and privacy protection with generous free data limits.",
         ImageId: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAN1RnmRDRJfYfsU8ZDfdyLBlvMb8LNsua4Q&s",
         category: "vpn",
-        status :'shared',
+        status: 'shared',
         plans: {
             "12": "1300",
         },
@@ -135,7 +135,7 @@ export const Pricelist = [
         Info: "Alt Balaji - Stream a variety of original web series, exclusive movies, and kids' content across multiple genres.",
         ImageId: "https://upload.wikimedia.org/wikipedia/en/thumb/8/86/Altbalaji_Logo.svg/1200px-Altbalaji_Logo.svg.png",
         category: "ott",
-        status :'shared',
+        status: 'shared',
         plans: {
             "6": "700",
             "12": "1200",
@@ -147,7 +147,7 @@ export const Pricelist = [
         Info: "Ganna Plus - Access over 30 million songs, enjoy high-quality music streaming, and download tracks for offline listening.",
         ImageId: "https://images.yourstory.com/cs/images/companies/CLOGOS23-16-1588054358977.png?fm=auto&ar=1:1&mode=fill&fill=solid&fill-color=fff",
         category: "music",
-        status :'private',
+        status: 'private',
         plans: {
             "1": "240",
             "6": "680",
@@ -160,7 +160,7 @@ export const Pricelist = [
         Info: "Hulu - Watch thousands of TV shows and movies, including exclusive series and current-season episodes.",
         ImageId: "https://i.ytimg.com/vi/wgcvJG9K4r0/maxresdefault.jpg",
         category: "ott",
-        status :'shared',
+        status: 'shared',
         plans: {
             "1": "290",
             "3": "800",
@@ -172,7 +172,7 @@ export const Pricelist = [
         Info: "SonyLiv - Stream live TV, watch the latest movies, TV shows, and sports without the need for a VPN.",
         ImageId: "https://cdn-bgp.bluestacks.com/BGP/us/gametiles_com.sonyliv.jpg",
         category: "ott",
-        status :'shared',
+        status: 'shared',
         plans: {
             "6": "900",
             "12": "1400"
@@ -184,7 +184,7 @@ export const Pricelist = [
         Info: "Crunchyroll - Watch the latest anime series, classic shows, and simulcasts from Japan in high definition.",
         ImageId: "https://res.cloudinary.com/bsingh6636/image/upload/v1722090802/page/ott%20icons/crunchyroll_icon_w3qndn.jpg",
         category: "ott",
-        status :'shared',
+        status: 'shared',
         plans: {
             "1": "170",
             "3": "450",
@@ -197,7 +197,7 @@ export const Pricelist = [
         Info: "Kuku FM - Listen to a wide range of audiobooks, podcasts, and educational content in multiple Indian languages.",
         ImageId: "https://www.medianews4u.com/wp-content/uploads/2022/06/Kuku-FM-crosses-1-mn-active-paying-subscribers-in-India.jpg",
         category: "music",
-        status :'private',
+        status: 'private',
         plans: {
             "12": "1800",
         },
@@ -208,7 +208,7 @@ export const Pricelist = [
         Info: "DangalPlay - Access exclusive shows, regional content, and live TV channels with a focus on Indian entertainment.",
         ImageId: "https://res.cloudinary.com/bsingh6636/image/upload/v1718886363/page/ott%20icons/dangalPlay_egalcd.png",
         category: "ott",
-        status :'shared',
+        status: 'shared',
         plans: {
             "3": "499",
             "12": "1200",
@@ -220,7 +220,7 @@ export const Pricelist = [
         Name: "Express VPN",
         ImageId: "https://www.cnet.com/a/img/resize/b1cb20603ffb29a2501262328b981eabff61fb4f/hub/2021/01/19/6525e82f-c20c-477d-b8c4-26d5f06be6aa/expressvpn-vertical-logo-white-on-red.png?auto=webp&fit=crop&height=675&width=1200",
         category: "vpn",
-        status :'shared',
+        status: 'shared',
         Info: "A virtual private network service.",
         plans: {
             "1": "280",
@@ -234,7 +234,7 @@ export const Pricelist = [
         ImageId: "https://i.pinimg.com/474x/6c/d7/20/6cd7205407c8d4a5a7c4d5edad97cda5.jpg",
         category: "software",
         Info: "A graphic design platform, used to create social media graphics, presentations, posters, documents, and other visual content.",
-        status :'private',
+        status: 'private',
         plans: {
             "12": "250",
         },
@@ -245,7 +245,7 @@ export const Pricelist = [
         ImageId: "https://www.adgully.com/img/800/201906/zee5.jpg",
         category: "ott",
         Info: "An Indian subscription video-on-demand and over-the-top streaming service.",
-        status :'shared',
+        status: 'shared',
         plans: {
             "6": "850",
             "12": "1400",
@@ -257,7 +257,7 @@ export const Pricelist = [
         ImageId: "https://v3img.voot.com/v3Storage/assets/jc_logo_stack-1682163295957.jpg",
         category: "ott",
         Info: "A streaming service offering movies, TV shows, and more.",
-        status :'shared',
+        status: 'shared',
         plans: {
             "6": "600",
             "12": "1000",
@@ -268,7 +268,7 @@ export const Pricelist = [
         Name: "IDM",
         ImageId: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3eZG11q5NzSjezSmb5AF7N7VCzVJm_I_H78X2M4andw&s",
         category: "software",
-        status :'private',
+        status: 'private',
         Info: "Internet Download Manager is a tool to increase download speeds, resume, and schedule downloads.",
         // plans: {
         //     "1": "2250",
@@ -280,7 +280,7 @@ export const Pricelist = [
         Id: "20",
         Name: "Facebook Services",
         Info: "Face post like , comments . Story likes and comments",
-        status:'undefined',
+        status: 'undefined',
         ImageId: "https://store-images.s-microsoft.com/image/apps.37935.9007199266245907.b029bd80-381a-4869-854f-bac6f359c5c9.91f8693c-c75b-4050-a796-63e1314d18c9?h=464",
 
     },
@@ -289,7 +289,7 @@ export const Pricelist = [
         Name: "Instagram Services",
         Info: "Instagram followers, post likes and comments . story views and likes",
         ImageId: "https://img.freepik.com/free-vector/instagram-logo_1199-122.jpg",
-        status:'undefined',
+        status: 'undefined',
         category: "software",
 
     },
@@ -298,7 +298,7 @@ export const Pricelist = [
         Name: "Youtube Services",
         Info: "Monitize Youtube channel , youtube views , watchTime ,subscirbers , likes",
         ImageId: "https://feeds.abplive.com/onecms/images/uploaded-images/2022/01/31/0dfe05d1f843d2705c096b93ccb80e54_original.jpg?impolicy=abp_cdn&imwidth=720",
-        status:'undefined',
+        status: 'undefined',
         category: "software",
 
     },
@@ -308,7 +308,7 @@ export const Pricelist = [
         Info: "Offering you the most exclusive adult HD videos on the web! With over 2500 pornstars starring in 10,000+ scenes, you'll have access to all the big tits, hot asses and anal you could ever dream!. Watch exclusive sex videos. ",
         ImageId: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/brazzers-logo-betran-firmanza.jpg",
         category: "18+",
-        status:'undefined',
+        status: 'undefined',
         plans: {
             "1": "500",
             "3": "1200",
@@ -320,7 +320,7 @@ export const Pricelist = [
         Info: "Pornhub Premium is the ultimate source for HD porn videos featuring your favorite pornstars without ads. Enjoy the hottest premium pornhub videos online now!",
         ImageId: "https://is.zobj.net/image-server/v1/images?r=82U_12Cz0-c1mVwRTWdglB_Di6ZwJACjx8IEC8CRIlYsirvL1_FQayaEkubtASR4G9r_x6QEaBQy8fMMQR9QYZx1BUctNBPilLYh6WEckIQCQcnyzsoRbG2OhVIbODN9sctVslJT3X02IjpR2mXraVCi5jIKgUCgaTSmI3BVTeKweZtt1AIwlNeW1SX51ZFWJBeOoKAavSFK-9dhRPMHLxe5URt-RRtk8SNn0g",
         category: "18+",
-        status:'undefined',
+        status: 'undefined',
         plans: {
             "1": "500",
             "3": "1200",
@@ -332,7 +332,7 @@ export const Pricelist = [
         Info: "Home of HD reality porn featuring the nicest tits and ass online! The hottest curvy girls in real amateur sex stories are only on REALITYkings.com.",
         ImageId: "https://assets.change.org/photos/1/rn/kl/INRnkLFrpDWmVyt-800x450-noPad.jpg?1583109162",
         category: "18+",
-        status:'undefined',
+        status: 'undefined',
         plans: {
             "1": "500",
             "3": "1200",
@@ -344,7 +344,7 @@ export const Pricelist = [
         Info: "Naughty America is an independent pornographic film studio based in San Diego, California. Naughty America. Company type, Private. Industry, Pornography.",
         ImageId: "https://rukminim2.flixcart.com/image/850/1000/poster/b/g/e/naughty-america-sortedd-pgftcd065738-medium-original-imaedp3bbhbah88n.jpeg?q=90&crop=false",
         category: "18+",
-        status:'undefined',
+        status: 'undefined',
         plans: {
             "1": "500",
             "3": "1200",
@@ -356,7 +356,7 @@ export const Pricelist = [
         Info: "Here at Bang Bros, explore the highest quality porn with the sexiest amateur girls and the top pornstars. Enjoy fresh porn daily on Bangbros.com.",
         ImageId: "https://premiumatcheap.in/wp-content/uploads/2024/02/bang-bros-symbol-cropped-300x300-65bd319f2bd57.webp",
         category: "18+",
-        status:'undefined',
+        status: 'undefined',
         plans: {
             "1": "500",
             "3": "1200",
@@ -384,49 +384,49 @@ export const Category = [
         imgurl: "https://res.cloudinary.com/bsingh6636/image/upload/v1716583570/page/software-Logo_bzticr.png"
     },
     {
-        name : "games" ,
-        imgurl : "https://graphicsfamily.com/wp-content/uploads/edd/2021/09/Gaming-Logo-Design-Template-1180x664.jpg"
+        name: "games",
+        imgurl: "https://graphicsfamily.com/wp-content/uploads/edd/2021/09/Gaming-Logo-Design-Template-1180x664.jpg"
     }
 ]
 export const oldCarouselItem = [
-  {
-    id: 1,
-    image: "https://res.cloudinary.com/bsingh6636/image/upload/v1717157641/page/20240529035057_pan_ux15zj.avif",
-    title: "Panchayat",
-    description: "Get Prime Video at 20% OFF",
-    badge: "HOT DEAL",
-    ctaText: "Get Offer"
-  },
-  {
-    id: 2,
-    image: "https://res.cloudinary.com/bsingh6636/image/upload/v1716585466/page/ott_mvrnlk.png",
-    title: "All OTT Platforms",
-    description: "Bundle & save up to 40% on subscriptions",
-    badge: "TRENDING",
-    ctaText: "View Plans"
-  },
-  {
-    id: 3,
-    image: "https://res.cloudinary.com/bsingh6636/image/upload/v1716585467/page/music_xf8x5o.png",
-    title: "Music Streaming",
-    description: "Premium accounts at lowest prices",
-    badge: "NEW",
-    ctaText: "Subscribe Now"
-  },
-  {
-    id: 4,
-    image: 'https://res.cloudinary.com/bsingh6636/image/upload/v1717101257/page/SUSBCRIPTION-NEPAL_eddy09.png',
-    title: "Netflix Premium",
-    description: "Ultra HD streaming starting at ₹199/month",
-    badge: "BESTSELLER",
-    ctaText: "Join Now"
-  },
-  {
-    id: 5,
-    image: 'https://www.koimoi.com/wp-content/new-galleries/2021/09/kota-factory-season-2-review-002.jpg',
-    title: "Kota Factory",
-    description: "Exclusive student offers on all streaming plans",
-    badge: "LIMITED TIME",
-    ctaText: "Grab Deal"
-  }
+    {
+        id: 1,
+        image: "https://res.cloudinary.com/bsingh6636/image/upload/v1717157641/page/20240529035057_pan_ux15zj.avif",
+        title: "Panchayat",
+        description: "Get Prime Video at 20% OFF",
+        badge: "HOT DEAL",
+        ctaText: "Get Offer"
+    },
+    {
+        id: 2,
+        image: "https://res.cloudinary.com/bsingh6636/image/upload/v1716585466/page/ott_mvrnlk.png",
+        title: "All OTT Platforms",
+        description: "Bundle & save up to 40% on subscriptions",
+        badge: "TRENDING",
+        ctaText: "View Plans"
+    },
+    {
+        id: 3,
+        image: "https://res.cloudinary.com/bsingh6636/image/upload/v1716585467/page/music_xf8x5o.png",
+        title: "Music Streaming",
+        description: "Premium accounts at lowest prices",
+        badge: "NEW",
+        ctaText: "Subscribe Now"
+    },
+    {
+        id: 4,
+        image: 'https://res.cloudinary.com/bsingh6636/image/upload/v1717101257/page/SUSBCRIPTION-NEPAL_eddy09.png',
+        title: "Netflix Premium",
+        description: "Ultra HD streaming starting at ₹199/month",
+        badge: "BESTSELLER",
+        ctaText: "Join Now"
+    },
+    {
+        id: 5,
+        image: 'https://www.koimoi.com/wp-content/new-galleries/2021/09/kota-factory-season-2-review-002.jpg',
+        title: "Kota Factory",
+        description: "Exclusive student offers on all streaming plans",
+        badge: "LIMITED TIME",
+        ctaText: "Grab Deal"
+    }
 ];
